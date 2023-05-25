8783ffd118fe467d61a3a4aefc2484431f1bbdc8
dhefesson@gmail.com <dhefesson@gmail.com>
2023-05-25 16:59:14 +0000

    Olá Mundo dia01
    Aula SQL Essentials

---
+++ Dia-01/Olá Mundo!.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+select 'Olá Mundo' as primComando
+
+-- COMMAND ----------
+
+SELECT * -- selecione todas as colunas
+FROM silver.pizza_query.pedido -- da tabela silver.pizza_query.pedido
